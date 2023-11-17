158514d36eede5a95d6c6941fd430dd02e076951
fretka <fretka46.02@gmail.com>
2023-11-17 21:38:01 +0100

    Verified GPG commits

--- RCoreRpHandler.lua
+++ RCoreRpHandler.lua
@@ -7,4 +7,5 @@ Script for handling Events and other things related to SCP:CB RolePlay
 Using this script withnout permission of creator is denied
 
 Created by fretka46 for Resurrection GC - EU-CENTRAL Server
+
 ]]--
